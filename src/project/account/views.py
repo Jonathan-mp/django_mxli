23a8b888ee70ed8d7f59e8b5271765aa550a87f0
Jonathan Munoz <=>
2020-10-23 01:46:04 -0700

    login: error-message

--- src/project/account/views.py
+++ src/project/account/views.py
@@ -9,10 +9,11 @@ from . import forms
 
 def login(request):
     template_name = "account/login.html"
-    form = forms.LoginForm(request.POST or None)
+    form = forms.LoginForm(request.POST or None, use_required_attribute=False)
     
     if request.method == 'POST':
-        return HttpResponse(f'POST Recibido!</br>{request.POST["username"]} : {request.POST["password"]}')
+        if form.is_valid():
+            return HttpResponse(f'POST Recibido!</br>{request.POST["username"]} : {request.POST["password"]}')
 
     context = {
         'form': form
